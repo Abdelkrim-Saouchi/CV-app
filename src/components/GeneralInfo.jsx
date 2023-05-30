a767bd3638acf8a6767fbc4c2f18b55ec8dca02e
Krimothiazine <krimouv1995@gmail.com>
2023-05-30 14:22:50 +0100

    Redisign Profile Component

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import phoneIcon from '../phone-rotary-svgrepo-com.svg';
+import emailIcon from '../email-svgrepo-com.svg';
 
 export default class GeneralInfo extends Component {
   render() {
     return (
       <div className="GeneralInfo">
         <h1>Krimothiazine</h1>
+        <p className="phone">
+          <img src={phoneIcon} alt="phone icon" /> 1254-324-214
+        </p>
+        <p className="email">
+          <img src={emailIcon} alt="email icon" /> example@gmail.com
+        </p>
       </div>
     );
   }
