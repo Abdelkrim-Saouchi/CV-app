98a9a227634f937f5f4e84383a7a250fa340a6b3
Krimothiazine <krimouv1995@gmail.com>
2023-05-30 15:35:09 +0100

    Redisign Experience Component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
-
+import timeIcon from '../calendar-1196-svgrepo-com.svg';
+import locationIcon from '../location-pin-svgrepo-com.svg';
 export default class Experience extends Component {
   render() {
     return (
       <div className="Experience">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam totam
-        tenetur libero repudiandae minus ipsam praesentium minima quo,
-        perspiciatis unde cumque sit accusantium ipsa optio!
+        <div className="exp one">
+          <h2>
+            <div className="job">Nice job</div>
+            <div className="company">Super Company</div>
+          </h2>
+          <div className="exp_info">
+            <div className="period">
+              <img src={timeIcon} alt="period icon" />
+              2015-2018
+            </div>
+            <div className="location">
+              <img src={locationIcon} alt="location icon" /> Lorem, ipsum.
+            </div>
+          </div>
+          <ul>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              beatae.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga qui
+              suscipit molestiae nisi.
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          </ul>
+        </div>
+        <div className="exp two">
+          <h2>
+            <div className="job">Very Good job</div>
+            <div className="company">Very Good Company</div>
+          </h2>
+          <div className="exp_info">
+            <div className="period">
+              <img src={timeIcon} alt="period icon" />
+              2018-presence
+            </div>
+            <div className="location">
+              <img src={locationIcon} alt="location icon" /> Lorem, ipsum.
+            </div>
+          </div>
+          <ul>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              beatae.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga qui
+              suscipit molestiae nisi.
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          </ul>
+        </div>
       </div>
     );
   }
