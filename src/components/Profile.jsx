a37258a24a330406a0612fcf5c6768dd6fe8cfeb
Krimothiazine <krimouv1995@gmail.com>
2023-05-30 14:51:46 +0100

    Redisign Profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,9 +4,17 @@ export default class Profile extends Component {
   render() {
     return (
       <div className="Profile">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        provident quisquam illo illum maxime! Minima delectus ad dolore dolores
-        temporibus doloremque cupiditate possimus aut maiores!
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+            quos.
+          </li>
+          <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor
+            reiciendis aut incidunt porro adipisci delectus?
+          </li>
+        </ul>
       </div>
     );
   }
