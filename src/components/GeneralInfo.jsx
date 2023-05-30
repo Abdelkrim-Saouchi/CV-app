befd1ecf7bc4dfec5b23d3b65c6b53f3029a8f47
Krimothiazine <krimouv1995@gmail.com>
2023-05-30 16:35:24 +0100

    Adjust Design

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -6,13 +6,15 @@ export default class GeneralInfo extends Component {
   render() {
     return (
       <div className="GeneralInfo">
-        <h1>Krimothiazine</h1>
-        <p className="phone">
-          <img src={phoneIcon} alt="phone icon" /> 1254-324-214
-        </p>
-        <p className="email">
-          <img src={emailIcon} alt="email icon" /> example@gmail.com
-        </p>
+        <h1>Your Name</h1>
+        <div className="info">
+          <p className="phone">
+            <img src={phoneIcon} alt="phone icon" /> 1254-324-214
+          </p>
+          <p className="email">
+            <img src={emailIcon} alt="email icon" /> example@gmail.com
+          </p>
+        </div>
       </div>
     );
   }
