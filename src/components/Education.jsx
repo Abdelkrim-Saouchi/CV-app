0cf46ad8232d9d9ebaf4c16dc33e0ca8e3622a56
Krimothiazine <krimouv1995@gmail.com>
2023-06-05 18:16:44 +0100

    Redesign Education Component with Expertise Component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import timeIcon from '../calendar-1196-svgrepo-com.svg';
-import Enrichment from './Enrichment';
+import Expertise from './Expertise';
 import uniqid from 'uniqid';
 
 export default class Education extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       career: [
         {
@@ -62,12 +61,171 @@ export default class Education extends Component {
         },
       ],
     };
+    this.setFormDisplay = this.setFormDisplay.bind(this);
+    this.changeKnowledgeInfoHandler =
+      this.changeKnowledgeInfoHandler.bind(this);
+    this.changeKnowledgeTasksHandler =
+      this.changeKnowledgeTasksHandler.bind(this);
+    this.addTask = this.addTask.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
+    this.deleteKnowledge = this.deleteKnowledge.bind(this);
+    this.addKnowledge = this.addKnowledge.bind(this);
   }
-  render() {
-    const { career } = this.state;
-    const eduContent = career.map((study) => {
-      return <Enrichment path={study} timeIcon={timeIcon} key={study.id} />;
+
+  setFormDisplay() {
+    this.setState({
+      formClassName:
+        this.state.formClassName === 'form experience_form hidden'
+          ? 'form experience_form'
+          : 'form experience_form hidden',
     });
-    return <div className="Education">{eduContent}</div>;
+  }
+
+  changeKnowledgeInfoHandler(e) {
+    const targetProp = e.target.id;
+    const knowledgeId = e.target.parentElement.parentElement.id;
+
+    const newCareerList = this.state.career.map((knowledge) => {
+      if (knowledge.id === knowledgeId) {
+        switch (targetProp) {
+          case 'knowledge':
+            knowledge.pathName = e.target.value;
+            break;
+          case 'place':
+            knowledge.place = e.target.value;
+            break;
+          case 'from':
+            knowledge.period.from = e.target.value;
+            break;
+          case 'to':
+            knowledge.period.to = e.target.value;
+            break;
+          case 'location':
+            knowledge.location = e.target.value;
+            break;
+        }
+      }
+      return knowledge;
+    });
+    this.setState({
+      career: [...newCareerList],
+    });
+  }
+
+  changeKnowledgeTasksHandler(e) {
+    const taskIndex = Number(e.target.id);
+    const knowledgeId = e.target.parentElement.parentElement.id;
+    const newCareerList = this.state.career.map((knowledge) => {
+      if (knowledge.id === knowledgeId) {
+        knowledge.tasks.map((task, index) => {
+          if (index === taskIndex) {
+            task.taskText = e.target.value;
+          }
+          return task;
+        });
+      }
+      return knowledge;
+    });
+
+    this.setState({
+      career: [...newCareerList],
+    });
+  }
+
+  addTask(e) {
+    e.preventDefault();
+    const newTaskText = e.target.previousSibling.value;
+    const knowledgeId = e.target.parentElement.parentElement.id;
+    const newCareerList = this.state.career.map((knowledge) => {
+      if (knowledge.id === knowledgeId) {
+        knowledge.tasks = [
+          ...knowledge.tasks,
+          { id: uniqid(), taskText: newTaskText },
+        ];
+      }
+      return knowledge;
+    });
+
+    this.setState({
+      career: [...newCareerList],
+    });
+    e.target.previousSibling.value = '';
+  }
+
+  deleteTask(e) {
+    const targetIndex = Number(e.target.previousSibling.id);
+    const knowledgeId = e.target.parentElement.parentElement.id;
+    const newCareerList = this.state.career.map((knowledge) => {
+      if (knowledge.id === knowledgeId) {
+        knowledge.tasks = knowledge.tasks.filter(
+          (item, index) => index !== targetIndex
+        );
+      }
+      return knowledge;
+    });
+
+    this.setState({
+      career: [...newCareerList],
+    });
+  }
+
+  deleteKnowledge(e) {
+    const knowledgeId = e.target.parentElement.id;
+    const newCareerList = this.state.career.filter(
+      (knowledge) => knowledge.id !== knowledgeId
+    );
+
+    this.setState({
+      career: [...newCareerList],
+    });
+  }
+
+  addKnowledge(e) {
+    const parent = e.target.parentElement;
+    const knowledge = parent.querySelector('#knowledge');
+    const place = parent.querySelector('#place');
+    const from = parent.querySelector('#from');
+    const to = parent.querySelector('#to');
+    const location = parent.querySelector('#location');
+
+    this.setState({
+      career: [
+        ...this.state.career,
+        {
+          id: uniqid(),
+          pathName: knowledge.value,
+          place: place.value,
+          period: { from: from.value, to: to.value },
+          location: location.value,
+          tasks: [],
+        },
+      ],
+    });
+
+    knowledge.value = '';
+    place.value = '';
+    from.value = '';
+    to.value = '';
+    location.value = '';
+  }
+
+  render() {
+    return (
+      <div className="Education">
+        <Expertise
+          handleFormDisplay={this.setFormDisplay}
+          changeKnowledgeInfoHandler={this.changeKnowledgeInfoHandler}
+          changeKnowledgeTasksHandler={this.changeKnowledgeTasksHandler}
+          addTask={this.addTask}
+          deleteTask={this.deleteTask}
+          deleteKnowledge={this.deleteKnowledge}
+          addKnowledge={this.addKnowledge}
+          componentState={this.state}
+          knowledge="Degree"
+          compTitle="Education"
+          place="University"
+        />
+      </div>
+    );
   }
 }
