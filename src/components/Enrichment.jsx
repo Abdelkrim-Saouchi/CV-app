f0b311650a9899d9899466108ce61aed78287790
Krimothiazine <krimouv1995@gmail.com>
2023-06-08 20:03:59 +0100

    Transform Enrichement to functional component

--- src/components/Enrichment.jsx
+++ src/components/Enrichment.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Enrichment extends Component {
-  render() {
-    const { path, timeIcon, children } = this.props; // I couldn't find a better common name for study career/job career, maybe I'll change it later
-    return (
-      <div className="enrich">
-        <h2>
-          <div className="Academy_work">{path.pathName}</div>
-          <div className="organization">{path.place}</div>
-        </h2>
-        <div className="enrich_info">
-          <div className="period">
-            <img src={timeIcon} alt="period icon" />
-            {path.period.from}-{path.period.to}
-          </div>
-          {children}
+// I couldn't find a better common name for study career/job career, maybe I'll change it later
+const Enrichment = ({ path, timeIcon, children }) => {
+  return (
+    <div className="enrich">
+      <h2>
+        <div className="Academy_work">{path.pathName}</div>
+        <div className="organization">{path.place}</div>
+      </h2>
+      <div className="enrich_info">
+        <div className="period">
+          <img src={timeIcon} alt="period icon" />
+          {path.period.from}-{path.period.to}
         </div>
-        <ul>
-          {path.tasks.map((task) => {
-            return <li key={task.id}>{task.taskText}</li>;
-          })}
-        </ul>
+        {children}
       </div>
-    );
-  }
-}
+      <ul>
+        {path.tasks.map((task) => {
+          return <li key={task.id}>{task.taskText}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Enrichment;
