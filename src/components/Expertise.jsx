b6183af54311557821583d7d9b39d379f35f4594
Krimothiazine <krimouv1995@gmail.com>
2023-06-08 21:17:05 +0100

    Transform Expertise to functional component

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import timeIcon from '../assets/calendar-1196-svgrepo-com.svg';
 import locationIcon from '../assets/location-pin-svgrepo-com.svg';
 import Enrichment from './Enrichment';
@@ -7,144 +7,147 @@ import Form from './Form';
 import deleteIcon from '../assets/delete-2-svgrepo-com.svg';
 import addIcon from '../assets/add-plus-square-svgrepo-com.svg';
 
-export default class Expertise extends Component {
-  render() {
-    const { career } = this.props.componentState;
+const Expertise = ({
+  componentState,
+  deleteKnowledge,
+  changeKnowledgeInfoHandler,
+  changeKnowledgeTasksHandler,
+  deleteTask,
+  addTask,
+  handleFormDisplay,
+  compTitle,
+  addKnowledge,
+  knowledge,
+  place,
+}) => {
+  const { career } = componentState;
+  const expertiseContent = career.map((knowledge) => {
+    return (
+      <Enrichment path={knowledge} timeIcon={timeIcon} key={knowledge.id}>
+        <div className="location">
+          <img src={locationIcon} alt="location icon" />
+          {knowledge.location}
+        </div>
+      </Enrichment>
+    );
+  });
 
-    const expertiseContent = career.map((knowledge) => {
-      return (
-        <Enrichment path={knowledge} timeIcon={timeIcon} key={knowledge.id}>
-          <div className="location">
-            <img src={locationIcon} alt="location icon" />
-            {knowledge.location}
-          </div>
-        </Enrichment>
-      );
-    });
+  const knowledgeContent = career.map((knldge) => {
+    return (
+      <div key={knldge.id} id={knldge.id} className="knowledge_container">
+        <img
+          src={deleteIcon}
+          id="delete_knowledge"
+          alt="delete knowledge icon"
+          onClick={deleteKnowledge}
+        />
+        <label htmlFor="knowledge">
+          {knowledge}
+          <input
+            type="text"
+            id="knowledge"
+            value={knldge.pathName}
+            onChange={changeKnowledgeInfoHandler}
+          />
+        </label>
+        <label htmlFor="place">
+          {place}:
+          <input
+            type="text"
+            id="place"
+            value={knldge.place}
+            onChange={changeKnowledgeInfoHandler}
+          />
+        </label>
+        <label htmlFor="from">
+          From:
+          <input
+            type="text"
+            id="from"
+            value={knldge.period.from}
+            onChange={changeKnowledgeInfoHandler}
+          />
+        </label>
+        <label htmlFor="to">
+          To:
+          <input
+            type="text"
+            id="to"
+            value={knldge.period.to}
+            onChange={changeKnowledgeInfoHandler}
+          />
+        </label>
+        <label htmlFor="location">
+          Location:
+          <input
+            type="text"
+            id="location"
+            value={knldge.location}
+            onChange={changeKnowledgeInfoHandler}
+          />
+        </label>
+        <p>Skills:</p>
+        {knldge.tasks.map((task, index) => {
+          return (
+            <div key={task.id} className="input_container">
+              <input
+                id={index}
+                value={task.taskText}
+                onChange={changeKnowledgeTasksHandler}
+              />
+              <img src={deleteIcon} alt="delete icon" onClick={deleteTask} />
+            </div>
+          );
+        })}
+        <div className="input_container">
+          <input id="add_input" />
+          <img src={addIcon} alt="add icon" onClick={addTask} />
+        </div>
+      </div>
+    );
+  });
 
-    const knowledgeContent = career.map((knowledge) => {
-      return (
-        <div
-          key={knowledge.id}
-          id={knowledge.id}
-          className="knowledge_container"
-        >
+  return (
+    <>
+      {expertiseContent}
+      <EditBtn formDisplayHandler={handleFormDisplay} />
+      <Form
+        title={`Edit ${compTitle}`}
+        className={componentState.formClassName}
+        formDisplayHandler={handleFormDisplay}
+      >
+        {knowledgeContent}
+        <div className="knowledge_container">
           <img
-            src={deleteIcon}
-            id="delete_knowledge"
-            alt="delete knowledge icon"
-            onClick={this.props.deleteKnowledge}
+            src={addIcon}
+            id="add_knowledge"
+            alt="add knowledge icon"
+            onClick={addKnowledge}
           />
           <label htmlFor="knowledge">
-            {this.props.knowledge}
-            <input
-              type="text"
-              id="knowledge"
-              value={knowledge.pathName}
-              onChange={this.props.changeKnowledgeInfoHandler}
-            />
+            {knowledge}
+            <input type="text" id="knowledge" />
           </label>
           <label htmlFor="place">
-            {this.props.place}:
-            <input
-              type="text"
-              id="place"
-              value={knowledge.place}
-              onChange={this.props.changeKnowledgeInfoHandler}
-            />
+            {place}:
+            <input type="text" id="place" />
           </label>
           <label htmlFor="from">
             From:
-            <input
-              type="text"
-              id="from"
-              value={knowledge.period.from}
-              onChange={this.props.changeKnowledgeInfoHandler}
-            />
+            <input type="text" id="from" />
           </label>
           <label htmlFor="to">
             To:
-            <input
-              type="text"
-              id="to"
-              value={knowledge.period.to}
-              onChange={this.props.changeKnowledgeInfoHandler}
-            />
+            <input type="text" id="to" />
           </label>
           <label htmlFor="location">
             Location:
-            <input
-              type="text"
-              id="location"
-              value={knowledge.location}
-              onChange={this.props.changeKnowledgeInfoHandler}
-            />
+            <input type="text" id="location" />
           </label>
-          <p>Skills:</p>
-          {knowledge.tasks.map((task, index) => {
-            return (
-              <div key={task.id} className="input_container">
-                <input
-                  id={index}
-                  value={task.taskText}
-                  onChange={this.props.changeKnowledgeTasksHandler}
-                />
-                <img
-                  src={deleteIcon}
-                  alt="delete icon"
-                  onClick={this.props.deleteTask}
-                />
-              </div>
-            );
-          })}
-          <div className="input_container">
-            <input id="add_input" />
-            <img src={addIcon} alt="add icon" onClick={this.props.addTask} />
-          </div>
         </div>
-      );
-    });
+      </Form>
+    </>
+  );
+};
 
-    return (
-      <>
-        {expertiseContent}
-        <EditBtn formDisplayHandler={this.props.handleFormDisplay} />
-        <Form
-          title={`Edit ${this.props.compTitle}`}
-          className={this.props.componentState.formClassName}
-          formDisplayHandler={this.props.handleFormDisplay}
-        >
-          {knowledgeContent}
-          <div className="knowledge_container">
-            <img
-              src={addIcon}
-              id="add_knowledge"
-              alt="add knowledge icon"
-              onClick={this.props.addKnowledge}
-            />
-            <label htmlFor="knowledge">
-              {this.props.knowledge}
-              <input type="text" id="knowledge" />
-            </label>
-            <label htmlFor="place">
-              {this.props.place}:
-              <input type="text" id="place" />
-            </label>
-            <label htmlFor="from">
-              From:
-              <input type="text" id="from" />
-            </label>
-            <label htmlFor="to">
-              To:
-              <input type="text" id="to" />
-            </label>
-            <label htmlFor="location">
-              Location:
-              <input type="text" id="location" />
-            </label>
-          </div>
-        </Form>
-      </>
-    );
-  }
-}
+export default Expertise;
