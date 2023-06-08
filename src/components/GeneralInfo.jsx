fefce08fab744d4dfcbb1ccb04e9067217876ce9
Krimothiazine <krimouv1995@gmail.com>
2023-06-08 19:18:08 +0100

    Transform GeneralInfo to functional component

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,92 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import phoneIcon from '../assets/phone-rotary-svgrepo-com.svg';
 import emailIcon from '../assets/email-svgrepo-com.svg';
 import Form from './Form';
 import EditBtn from './EditBtn';
 
-export default class GeneralInfo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      info: {
-        name: 'Your Name',
-        phone: '124-333-200',
-        email: 'example@gmail.com',
-      },
-      formClassName: 'form generalInfo_form hidden',
-    };
-    this.setFormDisplay = this.setFormDisplay.bind(this);
-  }
+const GeneralInfo = () => {
+  const [name, setName] = useState('Your Name');
+  const [phone, setPhone] = useState('124-333-200');
+  const [email, setEmail] = useState('example@gmail.com');
+  const [edit, setEdit] = useState(false);
 
-  setFormDisplay() {
-    this.setState({
-      formClassName:
-        this.state.formClassName === 'form generalInfo_form hidden'
-          ? 'form generalInfo_form'
-          : 'form generalInfo_form hidden',
-    });
-  }
+  const setFormDisplay = () => {
+    setEdit(!edit);
+  };
 
-  setNameInput(name) {
-    this.setState({
-      info: {
-        name: name,
-        phone: this.state.info.phone,
-        email: this.state.info.email,
-      },
-    });
-  }
+  return (
+    <div className="GeneralInfo">
+      <h1>{name}</h1>
 
-  setPhoneInput(phone) {
-    this.setState({
-      info: {
-        name: this.state.info.name,
-        phone: phone,
-        email: this.state.info.email,
-      },
-    });
-  }
-
-  setEmailInput(email) {
-    this.setState({
-      info: {
-        name: this.state.info.name,
-        phone: this.state.info.phone,
-        email: email,
-      },
-    });
-  }
-
-  render() {
-    return (
-      <div className="GeneralInfo">
-        <h1>{this.state.info.name}</h1>
-
-        <div className="info">
-          <p className="phone">
-            <img src={phoneIcon} alt="phone icon" /> {this.state.info.phone}
-          </p>
-          <p className="email">
-            <img src={emailIcon} alt="email icon" /> {this.state.info.email}
-          </p>
-        </div>
-        <EditBtn formDisplayHandler={this.setFormDisplay} />
-        {/* <button className="edit_btn" onClick={this.setFormDisplay}>
-          Edit
-        </button> */}
-
-        <Form
-          className={this.state.formClassName}
-          title="Edit General Info"
-          formDisplayHandler={this.setFormDisplay}
-        >
+      <div className="info">
+        <p className="phone">
+          <img src={phoneIcon} alt="phone icon" /> {phone}
+        </p>
+        <p className="email">
+          <img src={emailIcon} alt="email icon" /> {email}
+        </p>
+      </div>
+      <EditBtn formDisplayHandler={setFormDisplay} />
+      {edit && (
+        <Form title="Edit General Info" formDisplayHandler={setFormDisplay}>
           <label htmlFor="nameInput">
             Name:
             <input
               type="text"
               id="nameInput"
-              value={this.state.info.name}
-              onChange={(e) => this.setNameInput(e.target.value)}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </label>
           <label htmlFor="phoneInput">
@@ -94,8 +43,8 @@ export default class GeneralInfo extends Component {
             <input
               type="text"
               id="phoneInput"
-              value={this.state.info.phone}
-              onChange={(e) => this.setPhoneInput(e.target.value)}
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
             />
           </label>
           <label htmlFor="emailInput">
@@ -103,12 +52,14 @@ export default class GeneralInfo extends Component {
             <input
               type="email"
               id="emailInput"
-              value={this.state.info.email}
-              onChange={(e) => this.setEmailInput(e.target.value)}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </label>
         </Form>
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
+
+export default GeneralInfo;
